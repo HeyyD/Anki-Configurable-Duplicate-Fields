b4a025fbe38f10659620b2ff385c1b48a241409c
heidi.hati <heidi.hati@sulake.com>
2024-10-29 09:50:24 +0200

    Rename directory

--- __init__.py
+++ __init__.py
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .flex_dupe_checking import setup
+from .configurable_duplicate_fields import setup
 
 setup()

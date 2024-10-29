5487d5304f3193094264655107938eec33f00480
heidi.hati <heidi.hati@sulake.com>
2024-10-29 09:54:46 +0200

    Rename package

--- release.sh
+++ release.sh
@@ -8,11 +8,11 @@ TEMP_DIR=`mktemp -d`
 echo Using temp dir $TEMP_DIR
 cp __init__.py $TEMP_DIR
 cp manifest.json $TEMP_DIR
-mkdir $TEMP_DIR/flex_dupe_checking
-cp flex_dupe_checking/__init__.py $TEMP_DIR/flex_dupe_checking
+mkdir $TEMP_DIR/configurable_duplicate_fields
+cp configurable_duplicate_fields/__init__.py $TEMP_DIR/configurable_duplicate_fields
 pushd $TEMP_DIR
-zip -r anki_flex_dupe_checking.zip .
+zip -r configurable_duplicate_fields.zip .
 echo Moving package to $TARGET_DIR
-mv anki_flex_dupe_checking.zip $TARGET_DIR
+mv configurable_duplicate_fields.zip $TARGET_DIR
 popd
 rm -rf $TEMP_DIR
